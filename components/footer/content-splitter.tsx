316cecedd439e9733b548252b6807a5122a903db
Hakan Cimen <hakan.cimen@pimberly.com>
2022-11-11 18:57:11 +0000

    added content footer splitter

---
+++ components/footer/content-splitter.tsx
@@ -0,0 +1,3 @@
+export const FooterSplitter = () => {
+  return <div className="bg-slate-800 p-0.5"></div>;
+};
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,4 +1,5 @@
 import { FooterContent } from "./content";
+import { FooterSplitter } from "./content-splitter";
 
 export const Footer = () => {
   const loremIpsum = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa odio
@@ -9,7 +10,9 @@ export const Footer = () => {
   return (
     <footer className="flex flex-row py-4 px-12 fixed bottom-0 bg-slate-900">
       <FooterContent title="Find" content={loremIpsum} />
+      <FooterSplitter />
       <FooterContent title="Follow" content={loremIpsum} />
+      <FooterSplitter />
       <FooterContent title="Contact" content={loremIpsum} />
     </footer>
   );
