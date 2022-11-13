a1178bc14fd89499cb4b227364bfd6ff2e102e5b
Hakan Cimen <hakan.cimen@pimberly.com>
2022-11-13 22:57:17 +0000

    header props now camelCase

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,13 @@
-export interface HeaderProps {
-  Header: string
+export interface headerProps {
+  Header: string;
 }
 
-export const Header = ({Header}: HeaderProps) => {
+export const Header = ({ Header }: headerProps) => {
   return (
     <div className="mt-40 flex flex-row justify-center">
-      <h1 className="decoration-black text-3xl font-bold underline underline-offset-8 decoration-black">{Header}</h1>
+      <h1 className="decoration-black text-3xl font-bold underline underline-offset-8 decoration-black">
+        {Header}
+      </h1>
     </div>
-  )
-}
+  );
+};
