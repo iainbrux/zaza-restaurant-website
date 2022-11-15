1708bd143ddc705477c939c574e19c198b1076ae
iainbrux <me@iainbrux.com>
2022-11-15 18:28:13 +0000

    added jest testing into application

--- src/common/components/nav-bar/index.tsx
+++ src/common/components/nav-bar/index.tsx
@@ -4,29 +4,34 @@ import Link from "next/link";
 export const NavigationBar = () => {
   return (
     <header>
-      <nav className="flex flex-row items-center justify-center py-4 border-b-2 shadow-sm border-sky-500 fixed top-0 w-full bg-gradient-to-r from-violet-500 to-fuchsia-500s bg-black z-10">
+      <nav
+        className="flex flex-row items-center justify-center py-4 border-b-2 shadow-sm border-sky-500 fixed top-0 w-full bg-gradient-to-r from-violet-500 to-fuchsia-500s bg-black z-10"
+        data-testid="navigation"
+      >
         <Image
           alt=""
           className="absolute left-8"
+          data-testid="nav-logo"
           height={85}
           src="/logo-ipsum.svg"
           width={175}
         />
         <div>
-          <Link className="mx-4" href="/">
+          <Link className="mx-4" data-testid="nav-home" href="/">
             HOME
           </Link>
-          <Link className="mx-4" href="/menu">
+          <Link className="mx-4" data-testid="nav-menu" href="/menu">
             MENU
           </Link>
-          <Link className="mx-4" href="/about">
+          <Link className="mx-4" data-testid="nav-menu" href="/about">
             ABOUT
           </Link>
-          <Link className="mx-4" href="/contact">
+          <Link className="mx-4" data-testid="nav-contact" href="/contact">
             CONTACT
           </Link>
           <Link
             className="py-2 px-4 bg-cyan-500 rounded-full mx-2"
+            data-testid="nav-reservations"
             href="/reservations"
           >
             RESERVATIONS
