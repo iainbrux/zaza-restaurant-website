79b84166cdafbfddb1b90cbfe1377d4f4fe18d97
Jake Meredith <jakemeredith@jakes-mbp-2.lan>
2022-11-18 17:59:45 +0000

    layout for contact page

--- src/common/components/nav-bar/index.tsx
+++ src/common/components/nav-bar/index.tsx
@@ -5,7 +5,7 @@ export const NavigationBar = () => {
   return (
     <header>
       <nav
-        className="flex flex-row items-center justify-center py-4 border-b-2 shadow-sm border-sky-500 fixed top-0 w-full bg-gradient-to-r from-violet-500 to-fuchsia-500s bg-black z-10"
+        className="flex flex-row items-center justify-center py-4 border-b-2 shadow-sm border-sky-500 sticky top-0 w-full bg-gradient-to-r from-violet-500 to-fuchsia-500s bg-black z-10"
         data-testid="navbar"
       >
         <Image
