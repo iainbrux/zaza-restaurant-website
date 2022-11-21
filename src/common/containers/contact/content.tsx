c6839c0e356ae9fc0f065a03a40fc2e4fdc8099b
Hakan Cimen <hakan.cimen@pimberly.com>
2022-11-21 18:40:54 +0000

    changed styling - bg-color

--- src/common/containers/contact/content.tsx
+++ src/common/containers/contact/content.tsx
@@ -29,7 +29,7 @@ export const Content = () => {
           <div className="flex flex-col mb-3">
             <label htmlFor="contact-form-name">Name</label>
             <input
-              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none bg-orange-200"
+              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none"
               id="contact-form-name"
               placeholder="John Smith"
               type="text"
@@ -38,7 +38,7 @@ export const Content = () => {
           <div className="flex flex-col mb-3">
             <label htmlFor="contact-form-email">Email</label>
             <input
-              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none bg-orange-200"
+              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none"
               id="contact-form-email"
               placeholder="JohnSmith@mymail.com"
               type="text"
@@ -52,7 +52,7 @@ export const Content = () => {
           <div className="flex flex-col mb-3">
             <label htmlFor="contact-form-subject">Subject</label>
             <input
-              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none bg-orange-200"
+              className="mt-2 p-1.5 border rounded bg-gray-50 focus:outline-none"
               id="contact-form-subject"
               placeholder="Subject"
               type="text"
@@ -61,13 +61,13 @@ export const Content = () => {
           <div className="flex flex-col mb-3">
             <label htmlFor="contact-form-message">Message</label>
             <textarea
-              className="mt-2 p-1.5 max-h-[300px] min-h-[150px] border rounded bg-gray-50 focus:outline-none bg-orange-200"
+              className="mt-2 p-1.5 max-h-[300px] min-h-[150px] border rounded bg-gray-50 focus:outline-none"
               id="contact-form-message"
               placeholder="Message"
             ></textarea>
           </div>
           <div className="mt-[30px] flex flex-row justify-center">
-            <button className="border px-2 py-1 rounded bg-orange-200 hover:bg-orange-400">
+            <button className="border px-2 py-1 rounded hover:bg-orange-200">
               Submit
             </button>
           </div>
