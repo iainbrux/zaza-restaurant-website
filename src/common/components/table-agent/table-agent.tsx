1e06cd6e54c66bf7554041c16864686d0635fa44
Hakan Cimen <hakan.cimen@pimberly.com>
2022-12-01 22:05:18 +0000

    added table-agent component

---
+++ src/common/components/table-agent/table-agent.tsx
@@ -0,0 +1,12 @@
+export const TableAgent = () => {
+  return (
+    <>
+      <iframe
+        className="border-0 min-w-[300px] max-w-[350px] min-h-[636px]"
+        height="100%"
+        src="https://tableagent.com/iframe/zaza-mediterranean-bbq/v/medium/"
+        width="100%"
+      ></iframe>
+    </>
+  );
+};
--- src/common/containers/reservations/index.tsx
+++ src/common/containers/reservations/index.tsx
@@ -1,10 +1,12 @@
 import { Footer } from "../../components/footer";
 import { NavigationBar } from "../../components/nav-bar";
+import { TableAgent } from "../../components/table-agent/table-agent";
 
 export const Reservations = () => {
   return (
     <>
       <NavigationBar />
+      <TableAgent />
       <Footer />
     </>
   );
