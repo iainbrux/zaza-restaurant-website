4035925ab15205df4fc578f2daefe60b12a3e8f9
Hakan Cimen <hakan.cimen@pimberly.com>
2022-12-05 23:27:08 +0000

    changed width of resmio grid

--- src/common/components/resmio/index.tsx
+++ src/common/components/resmio/index.tsx
@@ -5,7 +5,7 @@ export const ResmioForm = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "//static.resmio.com/static/en-gb/widget.js#height=600&width=400&fontSize=16px&backgroundColor=%23ffffff&color=%23555555&linkBackgroundColor=%23f5a623&facebookLogin=false&newsletterSignup=false&commentsDisabled=false&id=zaza-mediterannean-bbq-2";
+      "//static.resmio.com/static/en-gb/widget.js#height=700&width=500&fontSize=16px&backgroundColor=%23ffffff&color=%23555555&linkBackgroundColor=%23f5a623&facebookLogin=false&newsletterSignup=false&commentsDisabled=false&id=zaza-mediterannean-bbq-2";
     script.async = true;
     document.body.append(script);
 
@@ -30,20 +30,25 @@ export const ResmioForm = () => {
         data-testid="contact-main-region"
       >
         <section
-          className="px-3 my-3 w-full lg:w-1/2 lg:my-0 flex flex-col align-center text-center"
+          className="px-3 my-3 w-full lg:w-1/2 lg:my-0 flex flex-col"
           data-testid="contact-section-1"
         >
-          <p>
-            Use the Resmio booking system to book in advance. You can book for
-            up to six people. If you have a party larger than six, please
-            contact us on 01925 657 287 or email us at contact@zazamedbbq.com
-          </p>
+          <div className="flex justify-center">
+            <p>
+              Use the Resmio booking system to book in advance. You can book for
+              up to six people. If you have a party larger than six, please
+              contact us on 01925 657 287 or email us at contact@zazamedbbq.com
+            </p>
+          </div>
         </section>
         <section
-          className="px-3 my-3 w-full lg:w-1/2 lg:my-0 flex flex-col align-center text-center"
+          className="px-3 my-3 w-full lg:w-1/2 lg:my-0 flex flex-col"
           data-testid="contact-section-1"
         >
-          <div id="resmio-zaza-mediterannean-bbq-2">Resmio Booking System</div>
+          <div
+            className="flex justify-center"
+            id="resmio-zaza-mediterannean-bbq-2"
+          ></div>
         </section>
       </div>
     </main>
