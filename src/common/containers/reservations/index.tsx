41ec38a7ab05ba4f881918d03301261a2e232a81
Hakan Cimen <hakan.cimen@pimberly.com>
2022-12-05 20:20:55 +0000

    added resmio reservation content

--- src/common/containers/reservations/index.tsx
+++ src/common/containers/reservations/index.tsx
@@ -1,10 +1,12 @@
 import { Footer } from "../../components/footer";
 import { NavigationBar } from "../../components/nav-bar";
+import { ResmioForm } from "../../components/resmio";
 
 export const Reservations = () => {
   return (
     <>
       <NavigationBar />
+      <ResmioForm />
       <Footer />
     </>
   );
